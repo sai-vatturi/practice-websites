61b1c3e5bac0124ae4726bed333eb7ed353ab57e
Sainadh V <sainadhvatturi@gmail.com>
2024-03-24 23:08:30 +0530

    Landing Page React

---
+++ landing-page-reactjs/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Logo from "../Assets/Logo.svg";
+
+export const Navbar = () => {
+    return (
+        <div>
+            {Logo}
+            Navbar
+        </div>
+    );
+};
