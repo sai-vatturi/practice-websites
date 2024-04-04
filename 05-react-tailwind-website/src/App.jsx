5873b2c473de286645560c8112821e55f5c617e8
Sainadh V <sainadhvatturi@gmail.com>
2024-04-04 17:43:48 +0530

    commit

--- 05-react-tailwind-website/src/App.jsx
+++ 05-react-tailwind-website/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Navbar from "./assets/Navbar";
+import Navbar from "./assets/components/Navbar";
+import Counter from "./assets/components/Counter";
 
 function App() {
     return (
