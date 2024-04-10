78bb9d899d594715a7f883f6462c1a8a676b94e0
Sainadh V <sainadhvatturi@gmail.com>
2024-04-10 14:22:07 +0530

    Analytics component start

--- 05-react-tailwind-website/src/assets/components/Navbar.jsx
+++ 05-react-tailwind-website/src/assets/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-import Counter from "./Counter";
 
 const Navbar = () => {
     const [nav, setNav] = useState(true);
@@ -45,7 +44,6 @@ const Navbar = () => {
                     <li className="p-2">Contact Us</li>
                 </ul>
             </div>
-            <Counter />
         </div>
     );
 };
