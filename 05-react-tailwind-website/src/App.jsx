268f42aba85b1f9d537e3594715d37b34a0cdea9
Sainadh V <sainadhvatturi@gmail.com>
2024-04-10 14:21:49 +0530

    Hero component added

--- 05-react-tailwind-website/src/App.jsx
+++ 05-react-tailwind-website/src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./assets/components/Navbar";
-import Counter from "./assets/components/Counter";
+import Hero from "./assets/components/Hero";
+import Analytics from "./assets/components/Analytics";
 
 function App() {
     return (
         <div>
             <Navbar />
+            <Hero />
+            <Analytics />
         </div>
     );
 }
---
+++ 05-react-tailwind-website/src/assets/components/Hero.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { ReactTyped, Typed } from "react-typed";
+
+function Hero() {
+    return (
+        <div
+            className="text-white max-w-[800px] flex flex-col w-full h-screen
+        mt-[-96px] mx-auto text-center justify-center"
+        >
+            <p className="text-[#00df9a] font-bold text-lg sm:text-2l md:text-2xl">
+                GROWING WITH DATA ANALYTICS
+            </p>
+            <h1 className="text-5xl md:text-7xl sm:text-5xl font-bold py-3 sm:py-4 md:py-5 ">
+                Grow with data.
+            </h1>
+            <div className="flex justify-center items-center">
+                <p className="text-2xl sm:text-2xl lg:text-3xl ">
+                    Fast, flexible financing for
+                </p>
+                <ReactTyped
+                    className="text-2xl font-bold ml-2 sm:text-3xl"
+                    strings={["BTB", "BTC", "SASS"]}
+                    typeSpeed={120}
+                    backSpeed={140}
+                    loop
+                />
+            </div>
+            <p className="text-gray-500 py-2 md:text-lg md:py-4">
+                Monitor your data analytics to increase revenue for BTB, BTC and
+                SASS platforms.
+            </p>
+        </div>
+    );
+}
+
+export default Hero;
