3a833b1100d0034e223fbb59ec1aa83e578a03e4
Sainadh V <sainadhvatturi@gmail.com>
2024-04-13 23:55:37 +0530

    Created footer component

--- 05-react-tailwind-website/src/App.jsx
+++ 05-react-tailwind-website/src/App.jsx
@@ -7,6 +7,7 @@ import Hero from "./assets/components/Hero";
 import Analytics from "./assets/components/Analytics";
 import NewsLetter from "./assets/components/NewsLetter";
 import Cards from "./assets/components/Cards";
+import Footer from "./assets/components/Footer";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
             <Analytics />
             <NewsLetter />
             <Cards />
+            <Footer />
         </div>
     );
 }
